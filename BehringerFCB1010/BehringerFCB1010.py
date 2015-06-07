a7a1319395b2770688df26d6c59b59e62b33508e
Mark Grimes <mark.grimes@bristol.ac.uk>
2015-06-07 21:00:11 +0100

    Add loop pedal functionality

--- BehringerFCB1010/BehringerFCB1010.py
+++ BehringerFCB1010/BehringerFCB1010.py
@@ -10,7 +10,22 @@ from _Framework.InputControlElement import MIDI_NOTE_TYPE, MIDI_CC_TYPE
 from _Framework.ButtonMatrixElement import ButtonMatrixElement
 from functools import partial
 import Live
+import threading, Queue
+import datetime
 
+
+class CallOnceListener(object):
+    """
+    Listener object that disconnects itself as soon as it has
+    been called once.
+    """
+    def __init__(self, functor, disconnectFunction ):
+        self.functor=functor
+        self.disconnectFunction=disconnectFunction
+    def __call__(self, *a, **k) :
+        self.functor( *a, **k )
+        self.disconnectFunction(self)
+        
 
 class BehringerFCB1010(ControlSurface):
     NUMBER_OF_CONTROLS = 10 # The number of normal pedal buttons on the board, not including specials like "up"
@@ -25,6 +40,13 @@ class BehringerFCB1010(ControlSurface):
     def __init__(self, *a, **k):
         super(BehringerFCB1010, self).__init__(*a, **k)
         self.log_message("Starting Behringer FCB1010 script __init__")
+        
+        self.tracksToDisarm=[]
+        # I can't figure out why, but I can't use clip slots as the key in a dict, I always get
+        # a key error when trying to retrieve. I'll store as a (key,value) tuple in a list and
+        # search manually. 
+        self.clipSlotTimers=[] # deletes the clip if it isn't cancelled before finishing. Key is the clip slot
+        self.buttonHoldTime=datetime.timedelta(0,2)
         # I've already set up the board so that it transmits note C#-2 for pedal 1, D-2 for pedal 3 and so on
         # (increasing each time by one semitone) the MIDI number for note C#-2 is 1 (hence why I chose it for
         # pedal 1). These are hard coded in the PEDAL_MIDI_NOTES array. Note that this is for bank 00. At the
@@ -37,34 +59,27 @@ class BehringerFCB1010(ControlSurface):
 
             self._selectButtons[0].add_value_listener( self.begin_recording_handler )
             self._selectButtons[1].add_value_listener( self.begin_recording_new_scene_handler )
+            self._selectButtons[2].add_value_listener( self.playCurrentRecordingAndArmNext )
+            self._selectButtons[3].add_value_listener( self.stopSong )
+            self._selectButtons[4].add_value_listener( self.toggleMetronome )
+            
 
-            self._selectButtons[5].add_value_listener( partial(self.fire_scene,sceneNumber=0) )
-            self._selectButtons[6].add_value_listener( partial(self.fire_scene,sceneNumber=1) )
-            self._selectButtons[7].add_value_listener( partial(self.fire_scene,sceneNumber=2) )
-            self._selectButtons[8].add_value_listener( partial(self.fire_scene,sceneNumber=3) )
-            self._selectButtons[9].add_value_listener( partial(self.fire_scene,sceneNumber=4) )
+#             self._selectButtons[5].add_value_listener( partial(self.fire_scene,sceneNumber=0) )
+#             self._selectButtons[6].add_value_listener( partial(self.fire_scene,sceneNumber=1) )
+#             self._selectButtons[7].add_value_listener( partial(self.fire_scene,sceneNumber=2) )
+#             self._selectButtons[8].add_value_listener( partial(self.fire_scene,sceneNumber=3) )
+#             self._selectButtons[9].add_value_listener( partial(self.fire_scene,sceneNumber=4) )
+            self._selectButtons[5].add_value_listener( partial(self.fire_clip,clipNumber=0) )
+            self._selectButtons[6].add_value_listener( partial(self.fire_clip,clipNumber=1) )
+            self._selectButtons[7].add_value_listener( partial(self.fire_clip,clipNumber=2) )
+            self._selectButtons[8].add_value_listener( partial(self.fire_clip,clipNumber=3) )
+            self._selectButtons[9].add_value_listener( partial(self.fire_clip,clipNumber=4) )
             
             # Define a function to say where the start of the highlighted region is. When all
             # scripts are loaded I'll try and find another control surface and use that highlighted
             # region. If I can't find one just work relative to the first clip.
             self.scene_offset=lambda : 0
-
-            #self._clipLaunchButtons=[ [self._selectButtons[0],self._selectButtons[1],self._selectButtons[2] ],[self._selectButtons[5],self._selectButtons[6],self._selectButtons[7] ]]            
-#             self._matrix_buttons=[ [self._selectButtons[xIndex+yIndex*5] for xIndex in xrange(5) ] for yIndex in xrange(2) ]
-#             self._launchMatrix=ButtonMatrixElement( name='Button_Matrix', rows=self._matrix_buttons )
-#             self._session = SessionComponent(5, 2, auto_name=True, enable_skinning=False, is_enabled=False, layer=Layer(clip_launch_buttons=self._launchMatrix ) )
-#             self._mixer = MixerComponent( 5, auto_name=True, is_enabled=False, invert_mute_feedback=True)
-#             self._device=DeviceComponent(name='Device_Component', is_enabled=False)
-# 
-#             self._session.set_mixer( self._mixer )            
-#             self.set_device_component(self._device)
-#             self.set_highlighting_session_component(self._session)
-#             
-#             self._session.set_clip_launch_buttons(self._launchMatrix)
-#             self._session.scene(1).clip_slot(1).set_launch_button( self._selectButtons[0] )
-#             self._session.set_enabled(True)
-#             self._mixer.set_enabled(True)
-#             self._device.set_enabled(True)
+            self.track_offset=lambda : 0
 
         self.log_message("Finished Behringer FCB1010 script __init__")
 
@@ -85,6 +100,7 @@ class BehringerFCB1010(ControlSurface):
                 # This assumes the scene has the attribute name "_session" which may
                 # not be true, but it is for the control surface I'm using.
                 self.scene_offset=script._session.scene_offset
+                self.track_offset=script._session.track_offset
                 self.log_message("Behringer appears to have succesful piggy backed onto "+str(script))
                 break
             except Exception, error:
@@ -93,6 +109,7 @@ class BehringerFCB1010(ControlSurface):
         if self.scene_offset==None :
             self.log_message("Behringer couldn't find another control surface to piggy back onto. Scene control will only control the first scenes.")
             self.scene_offset=lambda : 0
+            self.track_offset=lambda : 0
 
     def get_matrix_button(self, column, row):
         return self._matrix_buttons[row][column]
@@ -114,6 +131,8 @@ class BehringerFCB1010(ControlSurface):
             # I assume the clip slots in a scene always match 1-to-1 with the number of tracks.
             # Start recording in the current scene for every track that is armed. If there is
             # a clip there already ignore that track. If anything is recording already, stop recording.
+            # This isn't quite the same as firing the scene, because any clips that aren't playing
+            # are left alone.
             for index in xrange( max( len(scene_slots), len(tracks) ) ) :
                 if tracks[index].arm or tracks[index].implicit_arm :
                     if (not scene_slots[index].has_clip) or scene_slots[index].is_recording :
@@ -169,3 +188,158 @@ class BehringerFCB1010(ControlSurface):
             index=self.scene_offset()+sceneNumber
             if index<len(scenes) :
                 scenes[index].fire()
+
+    def fire_clip(self, value, clipNumber ):
+        """
+        If the button is pressed, starts a timer and deletes the clip when the
+        timer finishes. If the button is released before the timer finishes then
+        it is cancelled and the clip fired.
+        I.e. press button once to fire clip, hold to delete current contents.
+        """
+        clipSlots=self.song().view.selected_scene.clip_slots
+        index=self.track_offset()+clipNumber
+        if index>=len(clipSlots) : return
+
+        if value>90 : # button has been pressed
+            self.log_message("index "+str(index))
+            if index<len(clipSlots) : self.log_message("clipSlots[index].has_clip= "+str(clipSlots[index].has_clip))
+            
+            if clipSlots[index].has_clip :
+                self.clipSlotTimers.append( (clipSlots[index],datetime.datetime.now()) )
+        else : # button has been released
+            timerWasRunning=False
+            # Manually search for the clip slot in the list of clipSlotTimers
+            self.log_message("Looking for timer in "+str(self.clipSlotTimers))
+            for (storedClipSlot,storedTimer) in self.clipSlotTimers :
+                if storedClipSlot==clipSlots[index] :
+                    self.log_message("Timer found")
+                    timerWasRunning=True#storedTimer.isAlive()
+                    self.log_message("Tomer was found="+str(timerWasRunning))
+                    self.clipSlotTimers.remove((storedClipSlot,storedTimer))
+                    break
+
+            if timerWasRunning or (not clipSlots[index].has_clip) : # button was released before clip deleted, so fire clip
+                with self.component_guard():
+                    if clipSlots[index].is_recording :
+                        # If recording just play the clip
+                        clipSlots[index].fire() # doesn't seem to be included in "else" for some reason. Maybe is_recording and is_triggered can both be true
+                    elif clipSlots[index].is_playing or clipSlots[index].is_triggered :
+                        clipSlots[index].stop()
+                    else :
+                        clipSlots[index].fire()
+
+    def deleteClip( self, clipSlot ) :
+        self.log_message("Deleting clip number "+str(clipSlot))
+        with self.component_guard():
+            clipSlot.delete_clip()
+                
+
+    def stopSong(self,value) :
+        if value<=90 : return # Only do this when the button is pressed (not released)
+        self.song().stop_playing()
+
+    def toggleMetronome( self, value ):
+        if value<=90 : return # Only do this when the button is pressed (not released)
+        self.song().metronome=not self.song().metronome
+
+    def playCurrentRecordingAndArmNext(self,value) :
+        """
+        Finds tracks that have the same input and output routing.
+        """
+        if value<=90 : return # Only do this when the button is pressed (not released)
+        # I need to know the index of the current scene. The only way I know to do this is
+        # to loop through all the scenes and see which one matches the current one
+        for sceneIndex in xrange( len(self.song().scenes) ) :
+            if self.song().scenes[sceneIndex]==self.song().view.selected_scene :
+                break
+
+        
+        allTracks=self.song().tracks
+        allClipSlots=self.song().view.selected_scene.clip_slots
+        tracksToDuplicate=[] # List of indices of tracks that where there wasn't a match found
+        for trackIndex in xrange( len(allClipSlots) ) :
+            if allTracks[trackIndex].arm==True or allTracks[trackIndex].implicit_arm==True :
+                # Make sure I don't do anything with any tracks that are in the process of
+                # finishing their recording.
+                if self.tracksToDisarm.count( allTracks[trackIndex] )!=0 : continue
+                # If it's currently recording then I want it to fire and start playing. If it's
+                # empty then I want it to fire and start recording.
+                if not allClipSlots[trackIndex].has_clip :
+                    allClipSlots[trackIndex].fire()
+                    continue # Can just record into here, so no need to find another track
+                if allClipSlots[trackIndex].is_recording :
+                    allClipSlots[trackIndex].fire()
+                if allClipSlots[trackIndex].has_clip :
+                    # If it previously had a clip, I want to find another track that has the same
+                    # input but an empty slot and start recording on that instead. I want it to use
+                    # the next available slot to the right; if there isn't one then loop around.
+                    foundMatchingTrack=False
+                    for secondTrackIndex in xrange( trackIndex+1, len(allClipSlots)+trackIndex ) : # start one to the right
+                        if secondTrackIndex>=len(allClipSlots) : secondTrackIndex-=len(allClipSlots) # loop around
+                        
+                        if not allClipSlots[secondTrackIndex].has_clip :
+                            # Check to see if the input is the same
+                            if allTracks[secondTrackIndex].current_input_routing==allTracks[trackIndex].current_input_routing \
+                            and allTracks[secondTrackIndex].current_input_sub_routing==allTracks[trackIndex].current_input_sub_routing :
+                                allTracks[secondTrackIndex].arm=True
+                                allClipSlots[secondTrackIndex].fire()
+                                # Can't unarm this track yet because recording stops immediately.
+                                # Need to add a listener to unarm once the playing status has changed.
+                                self.tracksToDisarm.append( allTracks[trackIndex] )
+                                foundMatchingTrack=True
+                                break
+                    if not foundMatchingTrack :
+                        # A suitable track wasn't found, so need to create a new one. Can't do this now
+                        # though because it would mess up this loop. Record the track index and do it
+                        # in a second loop.
+                        tracksToDuplicate.append(trackIndex)
+        
+        # Now I've looped over the pre-existing tracks, I can create any new ones that are
+        # required.
+        offsetFromNewTracks=0 # When I insert a track, the next items in the array will be wrong. Use this to keep track.
+        for sourceIndex in tracksToDuplicate :
+            sourceIndex+=offsetFromNewTracks # Note in python this doesn't affect the next iteration
+            # Can't use duplicate because it doesn't work if the current track is
+            # currently recording. Insert it immediately to the right
+            if allTracks[sourceIndex].has_audio_input :
+                self.song().create_audio_track(sourceIndex+1)
+            else :
+                self.song().create_midi_track(sourceIndex+1)
+            offsetFromNewTracks+=1 # Make sure the next iteration doesn't accidentally address the new track
+            # allTracks and allClipSlots don't appear to update with the new track, so get it again.
+            allTracks=self.song().tracks
+            allClipSlots=self.song().view.selected_scene.clip_slots
+
+            allTracks[sourceIndex+1].current_input_routing=allTracks[sourceIndex].current_input_routing
+            allTracks[sourceIndex+1].current_input_sub_routing=allTracks[sourceIndex].current_input_sub_routing
+            allTracks[sourceIndex+1].arm=True
+            allClipSlots[sourceIndex+1].fire() # Fire the new track
+            # Can't unarm this track yet because recording stops immediately.
+            # Need to delay until the recording has acually stopped. Can't use a listener
+            # because Live complains about making changes during notification. I'll just
+            # add it to a list of things that will be cleared up in a periodically function.
+            self.tracksToDisarm.append( allTracks[sourceIndex] )
+
+    def update_display(self,*a, **k):
+        super(BehringerFCB1010, self).update_display(*a, **k)
+        
+        # I can't disarm tracks in listeners so I have to check here to see if anything has
+        # been queued up to be disarmed. 
+        if len(self.tracksToDisarm)>0 :
+            # I need to know the index of the current scene. The only way I know to do this is
+            # to loop through all the scenes and see which one matches the current one
+            for sceneIndex in xrange( len(self.song().scenes) ) :
+                if self.song().scenes[sceneIndex]==self.song().view.selected_scene : break
+
+            for track in self.tracksToDisarm :
+                if not track.clip_slots[sceneIndex].is_recording :
+                    track.arm=False
+                    track.implicit_arm=False
+                    self.tracksToDisarm.remove(track)
+
+        if len(self.clipSlotTimers)>0 :
+            timeNow=datetime.datetime.now()
+            for (clipSlot,timeStarted) in self.clipSlotTimers[:] :
+                if timeNow-timeStarted > self.buttonHoldTime :
+                    clipSlot.delete_clip()
+                    self.clipSlotTimers.remove( (clipSlot,timeStarted) )
